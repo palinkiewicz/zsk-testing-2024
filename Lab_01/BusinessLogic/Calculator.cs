4ca7e7f12c4aab7f4c732f46d50953b1be496a20
palinkiewicz <pogromca.ap@gmail.com>
2024-09-23 14:43:53 +0200

    Add some docs

--- Lab_01/BusinessLogic/Calculator.cs
+++ Lab_01/BusinessLogic/Calculator.cs
@@ -2,11 +2,23 @@
 {
     public class Calculator
     {
+        /// <summary>
+        /// Sums two numbers together
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>The result of a + b</returns>
         public double Add(double a, double b)
         {
             return a + b;
         }
 
+        /// <summary>
+        /// Substracts the second number from the first number
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>The result of a - b</returns>
         public double Substract(double a, double b)
         {
             return a - b;
