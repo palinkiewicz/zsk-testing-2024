fddd5f6cf9355d0127a83eb6c69d66f31c277889
palinkiewicz <pogromca.ap@gmail.com>
2024-11-18 14:38:51 +0100

    Create Xamarin app and tests

---
+++ Lab_04_Xamarin/MobileApp.UITests/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MobileApp.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("pl.dakil.mobileapp").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ Lab_04_Xamarin/MobileApp/MobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using MobileApp.ViewModels;
+using Xamarin.Forms;
+
+namespace MobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
